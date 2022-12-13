f197c542205e4a9e4aeec8420a08b2c466feeaf1
Mikhail Sushchikh <sumux200013@gmail.com>
2022-12-13 20:09:19 +0400

    add tests

--- src/test/java/ShikimoriTest.java
+++ src/test/java/ShikimoriTest.java
@@ -1,65 +1,246 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.lang.reflect.Field;
+import java.time.Duration;
 
 
 public class ShikimoriTest {
     WebDriver driver;
     ShikimoriPage page;
 
-    @Before
+    @BeforeEach
     public void beforeMethod(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         page = new ShikimoriPage(driver);
         driver.get(page.URL);
     }
-    @After
+    @AfterEach
     public void afterMethod(){
         driver.quit();
     }
 
-
-    // 1. Тест: Открытие домашней страницы интернет-приложения.
+    // 3. Тест: Заполнение формы логина корректными данными и вход на домашнюю страницу для зарегистрированного пользователя.
     @Test
-    public void testOpenWebsite() throws NoSuchFieldException, IllegalAccessException {
+    public void testFillAuthorizationFormWithValidData() throws NoSuchFieldException, IllegalAccessException {
         //WHEN
-        Field xpathField = page.getClass().getDeclaredField("xpathToCheckWebsiteIsOpened");
-        xpathField.setAccessible(true);
-        String xpathToCheckWebsiteIsOpened = (String) xpathField.get(page);
 
-        String actual = page.getResultText(xpathToCheckWebsiteIsOpened);
-
-        //THEN
-        Assert.assertEquals(page.TEXT_TO_CHECK_WEBSITE_IS_OPENED, actual);
-    }
-
-    // 2. Тест: Открытие страницы с формой логина.
-    @Test
-    public void testOpenLoginPage() throws NoSuchFieldException, IllegalAccessException {
-        //WHEN
-        //Обращаемся к приватным полям класса ShikimoriPage
+        // Обращаемся к приватному полю с xpath на кнопку "Вход"
         Field xpathToLoginButtonField = page.getClass().getDeclaredField("xpathToLoginButton");
         xpathToLoginButtonField.setAccessible(true);
         String xpathToLoginButton = (String) xpathToLoginButtonField.get(page);
 
-        Field xpathToCheckLoginPageIsOpenedField = page.getClass().getDeclaredField("xpathToCheckLoginPageIsOpened");
-        xpathToCheckLoginPageIsOpenedField.setAccessible(true);
-        String xpathToCheckLoginPageIsOpened = (String) xpathToCheckLoginPageIsOpenedField.get(page);
+        // Обращаемся к приватному полю с xpath на input логина
+        Field xpathToInputLogin = page.getClass().getDeclaredField("xpathInputLogin");
+        xpathToInputLogin.setAccessible(true);
+        String xpathToLoginInput = (String) xpathToInputLogin.get(page);
+
+        // Обращаемся к приватному полю с xpath на input пароля
+        Field xpathToInputPassword = page.getClass().getDeclaredField("xpathInputPassword");
+        xpathToInputPassword.setAccessible(true);
+        String xpathToPasswordInput = (String) xpathToInputPassword.get(page);
+
+        // Обращаемся к приватному полю с xpath для кнопки "Войти"
+        Field xpathToSubmitButton = page.getClass().getDeclaredField("xpathToSubmitButton");
+        xpathToSubmitButton.setAccessible(true);
+        String xpathToSubmitInput = (String) xpathToSubmitButton.get(page);
+
+        // Обращаемся к приватному полю с xpath на никнейм профиля (он будет равен никнейму из логина)
+        Field xpathToCheckWebsiteIsOpenedForRegistered = page.getClass().getDeclaredField("xpathToCheckWebsiteIsOpenedForRegistered");
+        xpathToCheckWebsiteIsOpenedForRegistered.setAccessible(true);
+        String xpathToCheckWebsiteIsOpened = (String) xpathToCheckWebsiteIsOpenedForRegistered.get(page);
 
 
         page.clickButton(xpathToLoginButton);
-        Utilities.waiter();
-        String actual = page.getResultText(xpathToCheckLoginPageIsOpened);
+        Utilities.waiter(5000);
+
+        // page.clickButton(xpathToSpanCaptcha);
+        //Utilities.waiter(5000);
+
+        page.fillFormAndClickSubmit(xpathToLoginInput, page.TEXT_TO_FILL_LOGIN_INPUT,
+                                    xpathToPasswordInput, page.TEXT_TO_FILL_PASSWORD_INPUT,
+                                    xpathToSubmitInput);
+
+        // Utilities.waiter(9000);
+
+        // page.clickButton(xpathToSubmitInput);
+
+        String actual = page.getResultText(xpathToCheckWebsiteIsOpened);
 
         //THEN
-        Assert.assertEquals(page.TEXT_TO_CHECK_LOGIN_PAGE_IS_OPENED, actual);
+        Assertions.assertEquals(page.TEXT_TO_CHECK_WEBSITE_IS_OPENED_FOR_REGISTERED, actual, page.errorMessage);
+        driver.quit();
+    }
+
+    // 4. Тест: Заполнение формы логина некорректными данными (пустые поля).
+    @Test
+    public void testFillAuthorizationFormWithNoData() throws NoSuchFieldException, IllegalAccessException {
+        //WHEN
+
+        // Обращаемся к приватному полю с xpath на кнопку "Вход"
+        Field xpathToLoginButtonField = page.getClass().getDeclaredField("xpathToLoginButton");
+        xpathToLoginButtonField.setAccessible(true);
+        String xpathToLoginButton = (String) xpathToLoginButtonField.get(page);
+
+        // Обращаемся к приватному полю с xpath для кнопки "Войти"
+        Field xpathToSubmitButton = page.getClass().getDeclaredField("xpathToSubmitButton");
+        xpathToSubmitButton.setAccessible(true);
+        String xpathToSubmitInput = (String) xpathToSubmitButton.get(page);
+
+        // Обращаемся к приватному полю с xpath на сообщение об ошибке
+        Field xpathToCheckIfSignInCanceled = page.getClass().getDeclaredField("xpathToCheckIfSignInCanceled");
+        xpathToCheckIfSignInCanceled.setAccessible(true);
+        String xpathToCheckErrorMessageIsShown = (String) xpathToCheckIfSignInCanceled.get(page);
+
+
+        page.clickButton(xpathToLoginButton);
+        Utilities.waiter(10000);
+        page.clickButton(xpathToSubmitInput);
+        Utilities.waiter(1000);
+
+        String actual = page.getResultText(xpathToCheckErrorMessageIsShown);
+
+        //THEN
+        Assertions.assertEquals(page.TEXT_TO_CHECK_ERROR_MESSAGE_IS_SHOWN, actual, page.errorMessage);
+        driver.quit();
+    }
+
+    // 4.1 Тест: Заполнение формы логина некорректными данными (пустое поле пароля).
+    @Test
+    public void testFillAuthorizationFormWithNoPassword() throws NoSuchFieldException, IllegalAccessException {
+        //WHEN
+
+        // Обращаемся к приватному полю с xpath на кнопку "Вход"
+        Field xpathToLoginButtonField = page.getClass().getDeclaredField("xpathToLoginButton");
+        xpathToLoginButtonField.setAccessible(true);
+        String xpathToLoginButton = (String) xpathToLoginButtonField.get(page);
+
+        // Обращаемся к приватному полю с xpath на input логина
+        Field xpathToInputLogin = page.getClass().getDeclaredField("xpathInputLogin");
+        xpathToInputLogin.setAccessible(true);
+        String xpathToLoginInput = (String) xpathToInputLogin.get(page);
+
+        // Обращаемся к приватному полю с xpath для кнопки "Войти"
+        Field xpathToSubmitButton = page.getClass().getDeclaredField("xpathToSubmitButton");
+        xpathToSubmitButton.setAccessible(true);
+        String xpathToSubmitInput = (String) xpathToSubmitButton.get(page);
+
+        // Обращаемся к приватному полю с xpath на сообщение об ошибке
+        Field xpathToCheckIfSignInCanceled = page.getClass().getDeclaredField("xpathToCheckIfSignInCanceled");
+        xpathToCheckIfSignInCanceled.setAccessible(true);
+        String xpathToCheckErrorMessageIsShown = (String) xpathToCheckIfSignInCanceled.get(page);
+
+
+        page.clickButton(xpathToLoginButton);
+        Utilities.waiter(2000);
+        page.typeSomething(xpathToLoginInput, page.TEXT_TO_FILL_LOGIN_INPUT);
+        Utilities.waiter(10000);
+        page.clickButton(xpathToSubmitInput);
+        Utilities.waiter(1000);
+
+        String actual = page.getResultText(xpathToCheckErrorMessageIsShown);
+
+        //THEN
+        Assertions.assertEquals(page.TEXT_TO_CHECK_ERROR_MESSAGE_IS_SHOWN, actual, page.errorMessage);
+        driver.quit();
+    }
+
+    // 4.2 Тест: Заполнение формы логина некорректными данными (пустое поле логина).
+    @Test
+    public void testFillAuthorizationFormWithNoLogin() throws NoSuchFieldException, IllegalAccessException {
+        //WHEN
+
+        // Обращаемся к приватному полю с xpath на кнопку "Вход"
+        Field xpathToLoginButtonField = page.getClass().getDeclaredField("xpathToLoginButton");
+        xpathToLoginButtonField.setAccessible(true);
+        String xpathToLoginButton = (String) xpathToLoginButtonField.get(page);
+
+        // Обращаемся к приватному полю с xpath на input пароля
+        Field xpathToInputPassword = page.getClass().getDeclaredField("xpathInputPassword");
+        xpathToInputPassword.setAccessible(true);
+        String xpathToPasswordInput = (String) xpathToInputPassword.get(page);
+
+        // Обращаемся к приватному полю с xpath для кнопки "Войти"
+        Field xpathToSubmitButton = page.getClass().getDeclaredField("xpathToSubmitButton");
+        xpathToSubmitButton.setAccessible(true);
+        String xpathToSubmitInput = (String) xpathToSubmitButton.get(page);
+
+        // Обращаемся к приватному полю с xpath на сообщение об ошибке
+        Field xpathToCheckIfSignInCanceled = page.getClass().getDeclaredField("xpathToCheckIfSignInCanceled");
+        xpathToCheckIfSignInCanceled.setAccessible(true);
+        String xpathToCheckErrorMessageIsShown = (String) xpathToCheckIfSignInCanceled.get(page);
+
+
+        page.clickButton(xpathToLoginButton);
+        Utilities.waiter(2000);
+        page.typeSomething(xpathToPasswordInput, page.TEXT_TO_FILL_PASSWORD_INPUT);
+        Utilities.waiter(10000);
+        page.clickButton(xpathToSubmitInput);
+        Utilities.waiter(1000);
+
+        String actual = page.getResultText(xpathToCheckErrorMessageIsShown);
+
+        //THEN
+        Assertions.assertEquals(page.TEXT_TO_CHECK_ERROR_MESSAGE_IS_SHOWN, actual, page.errorMessage);
+        driver.quit();
+    }
+
+    // 5. Тест: Заполнение формы логина некорректными данными (рандомный логин, пароль).
+    @Test
+    public void testFillAuthorizationFormWithInValidData() throws NoSuchFieldException, IllegalAccessException {
+        //WHEN
+
+        // Обращаемся к приватному полю с xpath на кнопку "Вход"
+        Field xpathToLoginButtonField = page.getClass().getDeclaredField("xpathToLoginButton");
+        xpathToLoginButtonField.setAccessible(true);
+        String xpathToLoginButton = (String) xpathToLoginButtonField.get(page);
+
+        // Обращаемся к приватному полю с xpath на input логина
+        Field xpathToInputLogin = page.getClass().getDeclaredField("xpathInputLogin");
+        xpathToInputLogin.setAccessible(true);
+        String xpathToLoginInput = (String) xpathToInputLogin.get(page);
+
+        // Обращаемся к приватному полю с xpath на input пароля
+        Field xpathToInputPassword = page.getClass().getDeclaredField("xpathInputPassword");
+        xpathToInputPassword.setAccessible(true);
+        String xpathToPasswordInput = (String) xpathToInputPassword.get(page);
+
+        // Обращаемся к приватному полю с xpath для кнопки "Войти"
+        Field xpathToSubmitButton = page.getClass().getDeclaredField("xpathToSubmitButton");
+        xpathToSubmitButton.setAccessible(true);
+        String xpathToSubmitInput = (String) xpathToSubmitButton.get(page);
+
+        // Обращаемся к приватному полю с xpath на сообщение об ошибке
+        Field xpathToCheckIfSignInCanceled = page.getClass().getDeclaredField("xpathToCheckIfSignInCanceled");
+        xpathToCheckIfSignInCanceled.setAccessible(true);
+        String xpathToCheckErrorMessageIsShown = (String) xpathToCheckIfSignInCanceled.get(page);
+
+
+        page.clickButton(xpathToLoginButton);
+        Utilities.waiter(3000);
+
+        page.typeSomething(xpathToLoginInput, PasswordGenerator.generateRandomString("abcdefghijklmnopqrstuvwxyz", 12));
+        Utilities.waiter(3000);
+
+        page.typeSomething(xpathToPasswordInput, PasswordGenerator.generateStrongPassword());
+        Utilities.waiter(5000);
+
+        page.clickButton(xpathToSubmitInput);
+        Utilities.waiter(1000);
+
+        String actual = page.getResultText(xpathToCheckErrorMessageIsShown);
+
+        //THEN
+        Assertions.assertEquals(page.TEXT_TO_CHECK_ERROR_MESSAGE_IS_SHOWN, actual, page.errorMessage);
         driver.quit();
     }
 }

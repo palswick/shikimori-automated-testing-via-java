a7ac05b1f4395cdf96621ff1aa18f41c534f2bed
Mikhail Sushchikh <sumux200013@gmail.com>
2022-12-17 00:34:12 +0400

    refactor

--- src/test/java/ShikimoriTest.java
+++ src/test/java/ShikimoriTest.java
@@ -3,14 +3,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.lang.reflect.Field;
-import java.time.Duration;
 
 
 public class ShikimoriTest {
@@ -63,16 +59,9 @@ public class ShikimoriTest {
         page.clickButton(xpathToLoginButton);
         Utilities.waiter(5000);
 
-        // page.clickButton(xpathToSpanCaptcha);
-        //Utilities.waiter(5000);
-
         page.fillFormAndClickSubmit(xpathToLoginInput, page.TEXT_TO_FILL_LOGIN_INPUT,
                                     xpathToPasswordInput, page.TEXT_TO_FILL_PASSWORD_INPUT,
                                     xpathToSubmitInput);
-
-        // Utilities.waiter(9000);
-
-        // page.clickButton(xpathToSubmitInput);
 
         String actual = page.getResultText(xpathToCheckWebsiteIsOpened);
 

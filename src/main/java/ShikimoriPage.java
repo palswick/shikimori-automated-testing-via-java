41f00f261acceb67390fe74efdcd792b71831488
Mikhail Sushchikh <sumux200013@gmail.com>
2022-12-17 00:34:05 +0400

    refactor

--- src/main/java/ShikimoriPage.java
+++ src/main/java/ShikimoriPage.java
@@ -1,15 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class ShikimoriPage {
     WebDriver driver;
     public final String URL = "https://shikimori.one/";
-    public final String errorMessage = "Тест упал у вас... Соболезнуем.";
+    public final String errorMessage = "Your test is failed... Sorry for that :(";
 
 
     // 1. Данные: Открытие домашней страницы интернет-приложения.
@@ -66,17 +62,9 @@ public class ShikimoriPage {
         typeSomething(xpathForPassword, password);
         Utilities.waiter(3000);
 
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-//        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(
-//                By.xpath("//iframe[starts-with(@name, 'a-') and starts-with(@src, 'https://www.recaptcha.net/recaptcha/api2/')]")));
-//
-//        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='recaptcha-checkbox-border']"))).click();
 
         System.out.println("Clicked the checkbox");
 
-        // wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@class='btn-primary btn-submit btn']"))).click();
-
-        //wait.until(ExpectedConditions.elementToBeSelected(By.xpath("//input[@class='btn-primary btn-submit btn']")));
 
         clickButton(xpathForSubmitButton);
     }
